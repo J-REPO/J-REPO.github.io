0386367d0ba1fbb713aecf3d57a7b84ac26a6c32
sh4869 <nobuk4869@gmail.com>
2016-08-11 01:55:50 +0900

    Update gulpfile.js for using pug #9

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
-var haml = require('gulp-ruby-haml');
+var pug = require('gulp-pug');
 var plumber = require('gulp-plumber');
 var notifier = require('node-notifier');
 
 // hamlタスク設定
-gulp.task('haml', function() {
+gulp.task('pug', function() {
 	taskname = this.seq.slice(-1)[0]
-	gulp.src(['./*/*.haml','./*.haml'],{base:'./'})
+	gulp.src(['./*/*.pug','./*.pug'],{base:'./'})
 	.pipe(plumber({
 		errorHandler: function(error) {
 			var title = '[task]' + taskname + ' ' + error.plugin;
@@ -19,14 +19,14 @@ gulp.task('haml', function() {
 			});
 		}
 	}))
-	.pipe(haml({encodings: "UTF-8"}))
+	.pipe(pug({pretty: true}))
 	.pipe(gulp.dest('./'));
 });
 
 // 監視設定
 gulp.task('watch',function(){
-	gulp.watch(['./*/*.haml','./*.haml'],['haml']);
+	gulp.watch(['./*/*.pug','./*.pug'],['pug']);
 });
 
 // 項目追加
-gulp.task('default',['haml','watch']);
+gulp.task('default',['pug','watch']);
