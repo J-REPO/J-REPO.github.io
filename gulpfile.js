c0ceeec6681c4e9d2977b3b7f693f1219c52e77a
sh4869 <nobuk4869@gmail.com>
2016-09-06 14:56:54 +0900

    Separate section files

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('pug', function() {
 
 // 監視設定
 gulp.task('watch',function(){
-	gulp.watch(['./*/*.pug','./*.pug','./json/*.json'],['pug']);
+	gulp.watch(['./*/*.pug','./newui/**/*.pug','./*.pug','./json/*.json'],['pug']);
 });
 
 // 項目追加
