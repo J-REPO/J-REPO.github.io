6abe23ff6d1fbc57f54c61c0a7cd33ed65bb5bc1
sh4869 <nobuk4869@gmail.com>
2018-03-01 17:52:39 +0900

    Fix Article Generator

--- www/tools/article-generator.js
+++ www/tools/article-generator.js
@@ -1,39 +1,38 @@
 const fs = require('fs');
 const pug = require('pug');
-const path = require('path')
+const path = require('path');
+const mustache = require("mustache");
+
 const articles = JSON.parse(fs.readFileSync(__dirname + '/../json/articles.json', 'utf8'));
 
 const template_path = __dirname + '/../pug/template/article.pug';
 const html_ext = '.html';
 const md_ext = '.md';
 
-var buildTemplate = (path, variables) => {
-  var template = pug.compileFile(path, { pretty: true });
+try {
+  fs.mkdirSync(__dirname  + "/../../dest/article/");
+} catch(err){
+
+}
+
+var buildTemplate = (content, variables) => {
+  var template = pug.compile(content, { pretty: true ,filename: "www\\pug\\template\\article.pug"});
   var html = template({ option: variables });
   return html;
 };
 
 articles.forEach((article, index, array) => {
-  const mdfile_path = __dirname + '/../' + '/markdown' + article.url + md_ext;
-  // Copy text.md from article file
-  try {
-    fs.writeFileSync(__dirname + '/../pug/template/text.md', fs.readFileSync(mdfile_path));
-  } catch (err) {
-    console.log(err);
-  }
+  const mdfile_path = '..\\..\\markdown\\' + article.url + md_ext;
+  const view = {
+    md_url: mdfile_path
+  };
+  const output = mustache.render(fs.readFileSync(template_path).toString(),view);
   // Build Haml
-  html = buildTemplate(template_path, article);
-
-  fs.mkdir(__dirname + "/../../dest/article/", (err) => {
-    fs.mkdir(__dirname + "/../../dest/" + path.dirname(article.url), (err) => {
-      fs.writeFile(__dirname + "/../../dest/" + article.url + html_ext, html, (err) => {
-        fs.unlink(__dirname + '/../pug/template/text.md', (err) => {
-          if (err) {
-            console.log(err);
-          }
-        })
-      })
-    })
-  })
-
+  html = buildTemplate(output, article);
+  try {
+    fs.accessSync(__dirname + "/../../dest/" + path.dirname(article.url))
+  } catch (err){
+    fs.mkdirSync(__dirname + "/../../dest/" + path.dirname(article.url));
+  }
+  fs.writeFile(__dirname + "/../../dest/" + article.url + html_ext, html)
 });
