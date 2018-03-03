796a0e2a2be87b86eb9d5254322cdfd858f5acd7
sh4869 <nobuk4869@gmail.com>
2018-03-03 23:37:20 +0900

    Fix article generator bug

--- article-generator.js
+++ article-generator.js
@@ -9,16 +9,16 @@ const template_path = __dirname + '/www/pug/template/article.pug';
 const html_ext = '.html';
 const md_ext = '.md';
 
+// 必ずgulp上で実行されるので、destはすでにあると考えていい。
 try {
-  fs.mkdirSync(__dirname  + "/dest")
-  fs.mkdirSync(__dirname  + "/dest/article/");
-} catch(err){
-
+  fs.mkdirSync(__dirname + "/dest/article/");
+} catch (err) {
+  console.log(err)
 }
 
 var buildTemplate = (content, variables) => {
-  var template = pug.compile(content, { pretty: true ,filename: "www/pug/template/article.pug"});
-  var html = template({ option: variables });
+  var template = pug.compile(content, { pretty: true, filename: "www/pug/template/article.pug" });
+  var html = template(variables);
   return html;
 };
 
@@ -27,13 +27,15 @@ articles.forEach((article, index, array) => {
   const view = {
     md_url: mdfile_path
   };
-  const output = mustache.render(fs.readFileSync(template_path).toString(),view);
+  const output = mustache.render(fs.readFileSync(template_path).toString(), view);
   // Build Haml
   html = buildTemplate(output, article);
   try {
-    fs.accessSync(__dirname + "/dest" + path.dirname(article.url))
-  } catch (err){
-    fs.mkdirSync(__dirname + "/dest" + path.dirname(article.url));
+    fs.accessSync(__dirname + "/dest" + path.dirname(article.url));
+  } catch (err) {
+    if (err) {
+      fs.mkdirSync(__dirname + "/dest" + path.dirname(article.url));
+    }
   }
-  fs.writeFile(__dirname + "/dest" + article.url + html_ext, html)
+  fs.writeFileSync(__dirname + "/dest" + article.url + html_ext, html)
 });
